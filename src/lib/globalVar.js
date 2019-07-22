87555005113c2df65c8491f1aaf4c74ef033ab30
coldsewoo <coldsewoo@gmail.com>
2019-07-22 20:07:02 +0900

    updated backend API endpoint to aws EC2 instance

--- src/lib/globalVar.js
+++ src/lib/globalVar.js
@@ -1,4 +1,6 @@
-const API_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://coldsewooweb-backend.herokuapp.com'
+
+// dev - heroku backend (for testing)
+// prod - aws EC2 backend
+const API_URL = 'https://ec2-52-79-181-80.ap-northeast-2.compute.amazonaws.com:5000'
 
 export default API_URL
