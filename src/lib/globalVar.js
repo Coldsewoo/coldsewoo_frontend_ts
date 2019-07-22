eedfb5f39de1de698938cd93b9db8e402b67a8f9
coldsewoo <coldsewoo@gmail.com>
2019-07-22 22:31:03 +0900

    changed backend API endpoint to https

--- src/lib/globalVar.js
+++ src/lib/globalVar.js
@@ -1,6 +1,6 @@
 
 // dev - heroku backend (for testing)
 // prod - aws EC2 backend
-const API_URL = 'ec2-52-78-148-207.ap-northeast-2.compute.amazonaws.com'
+const API_URL = 'https://coldsewoo-backend.cf'
 
 export default API_URL
